09908922dab07c674995f3f130e28da7584bb9d9
siddharth2443 <siddharthghumare@gmail.com>
2023-05-04 01:51:36 +0530

    4-05-23

---
+++ 14) String/program174.java
@@ -0,0 +1,15 @@
+class Core {
+	public static void main(String[] args) {
+	
+		String str1 = "Core2Web";
+		String str2 = new String("Core2Web");
+
+		String str3 = "Core2Web";
+		String str4 = new String("Core2Web");
+
+		System.out.println(System.identityHashCode(str1));
+		System.out.println(System.identityHashCode(str2));
+		System.out.println(System.identityHashCode(str3));
+		System.out.println(System.identityHashCode(str4));
+	}
+}
---
+++ 14) String/program175.java
@@ -0,0 +1,11 @@
+class Core {
+	public static void main(String[] args) {
+	
+		String str1 = "Siddharth";		//scp
+		String str2 = "Siddharth";		//scp	
+		String str3 = new String("Siddharth");	//heap
+		String str4 = new String("Siddharth");	//heap
+		String str5 = new String("Sid");	//heap
+		String str6 = "Sid";			//scp
+	}
+}
